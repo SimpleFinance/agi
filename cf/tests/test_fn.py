b06323d176199696c935ca10e8929716fb6c32fa
Will Maier <wcmaier@m.aier.us>
2013-05-05 17:27:51 +0000

    Tests.

---
+++ cf/tests/test_fn.py
@@ -0,0 +1,51 @@
+import unittest
+
+from cf.fn import *
+
+class TestFn(unittest.TestCase):
+
+
+    def test_fn(self):
+        class FnTest(Fn): pass
+        self.assertTrue("Fn::Test" in FnTest("arg"))
+        self.assertEqual(
+            "arg",
+            FnTest("arg")["Fn::Test"])
+
+    def test_fn_base64(self):
+        self.assertEqual(
+            "arg",
+            FnBase64("arg")["Fn::Base64"])
+
+    def test_fn_find_in_map(self):
+        self.assertEqual(
+            ["map", "key", "label"],
+            FnFindInMap("map", "key", "label")["Fn::FindInMap"])
+
+    def test_fn_get_att(self):
+        self.assertEqual(
+            ["resource", "attr"],
+            FnGetAtt("resource", "attr")["Fn::GetAtt"])
+
+    def test_get_azs(self):
+        self.assertEqual(
+            "region",
+            FnGetAZs("region")["Fn::GetAZs"])
+
+    def test_fn_join(self):
+        self.assertEqual(
+            ["delimiter", "val1", "val2"],
+            FnJoin("delimiter", "val1", "val2")["Fn::Join"])
+
+    def test_fn_select(self):
+        self.assertEqual(
+            ["1", ["val1", "val2"]],
+            FnSelect(1, "val1", "val2")["Fn::Select"])
+
+    def test_ref(self):
+        self.assertEqual(
+            {"Ref": "name"},
+            Ref("name"))
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ cf/tests/test_template.py
@@ -0,0 +1,173 @@
+import unittest
+
+from cf.template import *
+
+class TestTemplate(unittest.TestCase):
+
+    def setUp(self):
+        super(TestTemplate, self).setUp()
+        self.template = Template(
+            AWSTemplateFormatVersion = "version",
+            Description = "description",
+            Parameters = "parameters",
+            Outputs = "outputs",
+            Mappings = "mappings",
+            Resources = dict(
+                Foo = "foo",
+            ),
+            # More Resources.
+            Bar = "bar",
+            Baz = "baz",
+        )
+
+    def test_version(self):
+        self.assertEqual("version", self.template["AWSTemplateFormatVersion"])
+
+    def test_description(self):
+        self.assertEqual("description", self.template["Description"])
+
+    def test_parameters(self):
+        self.assertEqual("parameters", self.template["Parameters"])
+
+    def test_mappings(self):
+        self.assertEqual("mappings", self.template["Mappings"])
+
+    def test_outputs(self):
+        self.assertEqual("outputs", self.template["Outputs"])
+
+    def test_resources_len(self):
+        self.assertEqual(3, len(self.template["Resources"]))
+
+    def test_resource(self):
+        self.assertEqual("foo", self.template["Resources"]["Foo"])
+
+
+class TestTemplateDefaults(unittest.TestCase):
+
+    def setUp(self):
+        super(TestTemplateDefaults, self).setUp()
+        self.template = Template(Foo = "foo")
+
+    def test_no_resources(self):
+        self.assertRaises(TypeError, Template)
+        
+    def test_skip_unset(self):
+        self.assertEqual(1, len(self.template))
+
+    def test_resource_len(self):
+        self.assertEqual(1, len(self.template))
+
+class TestResource(unittest.TestCase):
+
+    def setUp(self):
+        super(TestResource, self).setUp()
+        self.resource = Resource("type",
+            DependsOn = "dependson",
+            DeletionPolicy = "deletionpolicy",
+            UpdatePolicy = "updatepolicy",
+            Metadata = "metadata",
+            Properties = dict(
+                Foo = "foo",
+            ),
+            Bar = "bar",
+            Baz = "baz",
+            )
+
+    def test_depends_on(self):
+        self.assertEqual("dependson", self.resource["DependsOn"])
+
+    def test_deletion_policy(self):
+        self.assertEqual("deletionpolicy", self.resource["DeletionPolicy"])
+
+    def test_update_policy(self):
+        self.assertEqual("updatepolicy", self.resource["UpdatePolicy"])
+
+    def test_metadata(self):
+        self.assertEqual("metadata", self.resource["Metadata"])
+
+    def test_properties_len(self):
+        self.assertEqual(3, len(self.resource["Properties"]))
+
+    def test_property(self):
+        self.assertEqual("foo", self.resource["Properties"]["Foo"])
+
+class TestResourceDefaults(unittest.TestCase):
+
+    def setUp(self):
+        super(TestResourceDefaults, self).setUp()    
+        self.resource = Resource(Type = "type")
+
+    def test_nothing(self):
+        self.assertEqual(1, len(self.resource))
+
+    def test_type(self):
+        self.assertEqual("type", self.resource["Type"])
+
+    def test_no_type(self):
+        self.assertRaises(TypeError, Resource)
+
+
+class TestParameter(unittest.TestCase):
+
+    def setUp(self):
+        super(TestParameter, self).setUp()
+        self.parameter = Parameter("type",
+            Default="default",
+            AllowedValues="allowedvalues",
+            AllowedPattern="allowedpattern",
+            MaxLength="maxlength",
+            MinLength="minlength",
+            MaxValue="maxvalue",
+            MinValue="minvalue",
+            Description="description",
+            ConstraintDescription="constraintdescription",
+        )
+
+
+    def test_type(self):
+        self.assertEqual("type", self.parameter["Type"])
+        
+    def test_default(self):
+        self.assertEqual("default", self.parameter["Default"])
+
+    def test_allowedvalues(self):
+        self.assertEqual("allowedvalues", self.parameter["AllowedValues"])
+        
+    def test_allowedpattern(self):
+        self.assertEqual("allowedpattern", self.parameter["AllowedPattern"])
+        
+    def test_maxlength(self):
+        self.assertEqual("maxlength", self.parameter["MaxLength"])
+
+    def test_minlength(self):
+        self.assertEqual("minlength", self.parameter["MinLength"])
+        
+    def test_maxvalue(self):
+        self.assertEqual("maxvalue", self.parameter["MaxValue"])
+        
+    def test_minvalue(self):
+        self.assertEqual("minvalue", self.parameter["MinValue"])
+        
+    def test_description(self):
+        self.assertEqual("description", self.parameter["Description"])
+        
+    def test_constraintdescription(self):
+        self.assertEqual("constraintdescription", self.parameter["ConstraintDescription"])
+
+class TestParameterDefaults(unittest.TestCase):
+
+    def setUp(self):
+        super(TestParameterDefaults, self).setUp()
+        self.parameter = Parameter("type")
+
+    def test_nothing(self):
+        self.assertEqual(1, len(self.parameter))
+
+    def test_type(self):
+        self.assertEqual("type", self.parameter["Type"])
+
+    def test_no_type(self):
+        self.assertRaises(TypeError, Parameter)
+        
+if __name__ == "__main__":
+    unittest.main()
