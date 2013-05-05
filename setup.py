c634263e859c4d8d200237a068ad25543fb84157
Will Maier <wcmaier@m.aier.us>
2013-05-05 17:28:22 +0000

    Setup.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+meta = dict(
+    name             = "cf",
+    version          = "0.0.1",
+    packages         = ["cf"],
+    install_requires = [
+        ],
+    )
+
+setup(**meta)
