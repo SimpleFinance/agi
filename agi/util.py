8be09c3717e6df9a7c49ec1dded7050b6fbb38e8
Will Maier <wcmaier@m.aier.us>
2013-08-28 15:09:39 +0000

    Add a utility function to test for alphanumeric-ness.

--- agi/util.py
+++ agi/util.py
@@ -1,9 +1,14 @@
+import re
 from itertools import chain
 
-__all__ = ["filter_pairs", "merge"]
+__all__ = ["filter_pairs", "merge", "is_alpha"]
 
 def filter_pairs(**pairs):        
     return filter(lambda (k, v): v is not None, pairs.items())
 
 def merge(dicts):
     return dict(chain(*[d.items() for d in dicts]))
+
+alpha_re = re.compile("^[a-zA-Z0-9]+$")
+def is_alpha(string):
+    return True if alpha_re.match(string) else False
