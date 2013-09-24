867e18a70082462c82cb73c75386d39b0d76aebc
Matthew Hooker <mwhooker@gmail.com>
2013-09-24 16:51:50 -0700

    tests are not an agi submodule.

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup, find_packages
 
+
 setup(
-    name="agi",
-    version="0.0.1",
-    packages=find_packages(exclude=['agi.tests'])
+    name='agi',
+    version='0.0.1',
+    packages=find_packages(exclude=['tests'])
 )
