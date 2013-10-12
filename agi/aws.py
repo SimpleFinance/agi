298835328c1a93352f923b9cef71fb928da44c49
Will Maier <wcmaier@m.aier.us>
2013-10-12 22:10:22 +0000

    Update generated types list.

--- agi/aws.py
+++ agi/aws.py
@@ -16,18 +16,106 @@ def service(name, *services):
     return Service
 
 AWS = service("AWS")
+# Generate the following with:
+#   ./bin/pip install lxml
+#   ./bin/python -m agi.scripts.gen_types
 AWS.AutoScaling = service("AWS::AutoScaling",
-    "AutoScalingGroup",                          
+    "AutoScalingGroup",
     "LaunchConfiguration",
+    "ScalingPolicy",
+    "Trigger",
+)
+AWS.CloudFormation = service("AWS::CloudFormation",
+    "Authentication",
+    "CustomResource",
+    "Init",
+    "Stack",
+    "WaitCondition",
+    "WaitConditionHandle",
+)
+AWS.CloudFront = service("AWS::CloudFront",
+    "Distribution",
+)
+AWS.CloudWatch = service("AWS::CloudWatch",
+    "Alarm",
+)
+AWS.DynamoDB = service("AWS::DynamoDB",
+    "Table",
 )
 AWS.EC2 = service("AWS::EC2",
+    "CustomerGateway",
+    "DHCPOptions",
     "EIP",
     "EIPAssociation",
     "Instance",
+    "InternetGateway",
+    "NetworkAcl",
+    "NetworkAclEntry",
     "NetworkInterface",
+    "NetworkInterfaceAttachment",
+    "Route",
+    "RouteTable",
     "SecurityGroup",
+    "SecurityGroupEgress",
+    "SecurityGroupIngress",
+    "Subnet",
+    "SubnetNetworkAclAssociation",
+    "SubnetRouteTableAssociation",
+    "VPC",
+    "VPCDHCPOptionsAssociation",
+    "VPCGatewayAttachment",
+    "VPNConnection",
+    "VPNConnectionRoute",
+    "VPNGateway",
+    "Volume",
+    "VolumeAttachment",
+)
+AWS.ElastiCache = service("AWS::ElastiCache",
+    "CacheCluster",
+    "ParameterGroup",
+    "SecurityGroup",
+    "SecurityGroupIngress",
+    "SubnetGroup ",
+)
+AWS.ElasticBeanstalk = service("AWS::ElasticBeanstalk",
+    "Application",
+    "Environment",
+)
+AWS.ElasticLoadBalancing = service("AWS::ElasticLoadBalancing",
+    "LoadBalancer",
 )
 AWS.IAM = service("AWS::IAM",
+    "AccessKey",
+    "Group",
     "InstanceProfile",
+    "Policy",
     "Role",
+    "User",
+    "UserToGroupAddition",
+)
+AWS.RDS = service("AWS::RDS",
+    "DBInstance",
+    "DBParameterGroup",
+    "DBSecurityGroup",
+    "DBSecurityGroupIngress",
+    "DBSubnetGroup",
+)
+AWS.Route53 = service("AWS::Route53",
+    "RecordSet",
+    "RecordSetGroup",
+)
+AWS.S3 = service("AWS::S3",
+    "Bucket",
+    "BucketPolicy",
+)
+AWS.SDB = service("AWS::SDB",
+    "Domain",
+)
+AWS.SNS = service("AWS::SNS",
+    "Topic",
+    "TopicPolicy",
+)
+AWS.SQS = service("AWS::SQS",
+    "Queue",
+    "QueuePolicy",
 )
